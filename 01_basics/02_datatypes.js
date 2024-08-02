24a21288253d3e6db1a0853fd0003d988c02a432
Sujit Kumar <sujitku5619@gmail.com>
2024-08-02 14:54:04 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let name = "Sujit kumar"
+let age = 20
+let isLogged = false
+
+//boolean true/false
+//string => ""
+//bigint
+//null => standalone value
+//undefinded => value is not definded
+// number 2 to poer 53
+//symbol => unique
+
+//objects
+
+console.log(typeof isLogged) //typeof is used to see the data type in Javascript
