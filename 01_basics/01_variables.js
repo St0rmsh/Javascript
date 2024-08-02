508e77e7e21e0c04391b8595e70c9d8cea5ba815
Sujit Kumar <sujitku5619@gmail.com>
2024-08-02 14:23:57 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1287
+let accountEmail = "sujitkm@google.com"
+var accountpassword = "8823"
+accountCity = "Assam"
+
+let accountState;
+
+ // accountId = 9 // Not allowed
+ accountEmail = "suj@google.com"
+accountCity = "Guwhati"
+accountpassword ="0007"
+console.log(accountId);
+console.table([accountEmail,accountpassword,accountCity,accountState]);
