a52e621d2286c0e1099e5a94ae8fae0f1b549841
Sujit Kumar <sujitku5619@gmail.com>
2024-08-02 15:23:28 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,33 @@
+let num = false
+console.log(typeof num);
+
+
+
+let valueInNumber = Number(num)
+
+
+console.log(typeof valueInNumber);
+
+
+console.log(valueInNumber);
+
+
+// "33" => 33
+//"47suj" =>NaN
+// true =>1 ; false=>0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//1=> true ; 0 => false
+//"" => false
+//"sujit" =>true
+
+
+let number = 33
+let StringNumber = String(number)
+console.log(StringNumber);
+console.log(typeof StringNumber);
