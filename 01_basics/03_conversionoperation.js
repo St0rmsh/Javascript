aa9c6cd063d9b059770bb1a5d64ef97f0d36832b
Sujit Kumar <sujitku5619@gmail.com>
2024-08-03 04:06:33 +0000

    Conversionoperation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -31,3 +31,50 @@ let number = 33
 let StringNumber = String(number)
 console.log(StringNumber);
 console.log(typeof StringNumber);
+
+
+// ========== OPERATION ========
+
+let value = 3
+let negvalue = -3
+
+console.log(negvalue);
+
+
+let str = "Hi"
+let str2 = "User"
+
+let str3 = str + str2 
+
+console.log(str3);
+
+
+
+console.log(1+"2")
+
+
+
+let game = 100
+++game;
+
+console.log(game);
+
+
+// increment operation
+
+
+let num1 = 4
+let num2  = ++num1 ;
+console.log(num2);
+console.log(num1);
+
+
+
+let x = 5
+let y = x++
+console.log(x);
+
+console.log(y);
+
+
+
