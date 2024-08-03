a3219c307ad4c4f148a5e0f9274a8f792ea83857
Sujit Kumar <sujitku5619@gmail.com>
2024-08-03 14:03:38 +0000

    datatype-summary

---
+++ 01_basics/05_datatypes-summery.js
@@ -0,0 +1,64 @@
+// Primitive datatypes / call by me datatype
+
+// 7 types : String , Null , Undefined , Number , Boolean , Symbol , Bigint
+
+// String
+const name = "Sujit"
+
+//Null
+const val = null
+console.log(val);
+
+// Undefined
+
+let address;
+console.log(address);
+
+//Number
+const Num = 1234
+console.log(Num);
+
+
+//Boolean
+const IsloggedIn = true
+console.log(IsloggedIn);
+
+// Symbol
+
+const sym = Symbol("579")
+const ym = Symbol("579")
+ console.log(sym === ym);
+
+
+
+
+
+
+
+
+
+
+// Non - Primitive / reference
+
+// Array, Obejects ,Functions
+
+
+// Array
+const myarr = ["sk","dk","sk","ak","ak"];
+console.log(myarr);
+
+
+// object
+let myObj = {
+    name = sujit,
+    age = 20,
+}
+
+
+// Functions
+
+const myfunction = function(){
+    console.log("Hii");
+    
+}
+
