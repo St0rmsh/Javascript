9fddccb099125f13d101ac7f5817e10703ee93d0
Sujit Kumar <sujitku5619@gmail.com>
2024-08-03 15:26:15 +0000

    Strings

---
+++ 01_basics/06_Strings.js
@@ -0,0 +1,39 @@
+const name = "Sujit"
+const Roll_no = 60
+
+console.log(` Hello my name is ${name} and my Roll_no is ${Roll_no}`);
+
+
+const Gname = new String("Storm")
+console.log(Gname.charAt(0));
+console.log(Gname.length);
+console.log(Gname.indexOf("m"));
+
+
+const NewString = Gname.slice(0,2)
+console.log(NewString);
+
+const now = Gname.substring(0,5)
+console.log(now);
+
+
+const Stringone = "suji"
+console.log(Stringone);
+console.log(Stringone.trim());
+console.log(Stringone.replace("uji","ujit"));
+console.log(Stringone.includes("sujit"));
+
+
+const two = "dgfwvuytackbydsigfvibutwv4ifubtivwrcbwiucw"
+console.log(two.split('t'));
+
+
+
+
+
+
+
+
+
+
+
