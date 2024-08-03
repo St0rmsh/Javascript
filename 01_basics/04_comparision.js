dd140e1aedcfb30378ac43acac0cf46f6805f094
Sujit Kumar <sujitku5619@gmail.com>
2024-08-03 04:38:16 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log("02">1);
+
+
+// note * comarision should be done with same data type
+
+
+console.log(null > 0);
+console.log(null >= 0);
+ console.log(null == 0);
+console.log(null < 0);
+console.log(null <= 0);
+
+console.log(undefined ==0);
+
+
+// === 
+
+console.log(5 === 5);
+
