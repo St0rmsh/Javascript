fef212f72cfd8caa5409450ea842c85bbbe0c978
Sujit Kumar <sujitku5619@gmail.com>
2024-08-04 16:47:32 +0000

    Array

---
+++ 02_basics/01_Array.js
@@ -0,0 +1,66 @@
+// Array
+
+const myArr = [3,6,8,10,6]
+console.log(myArr);
+
+const Arr = new Array (3,5,7,3,5)
+console.log(Arr);
+
+
+// Arr.push(4);
+
+// Arr.unshift(4);
+
+// Arr.shift();
+
+console.log(Arr.includes(10));
+
+console.log(Arr.indexOf(6));
+
+
+const neuArray = Arr.join()
+console.log(neuArray);
+
+console.log(Arr);
+
+console.log(typeof neuArray);
+
+console.log(typeof Arr);
+
+
+
+// Slice  and Splice
+
+console.log("A",Arr);
+console.log();
+
+
+
+
+
+const nArray = Arr.slice(0,2)
+
+
+
+
+console.log(nArray);
+console.log("B",Arr);
+
+
+
+
+const Arra = Arr.splice(0,3)
+console.log("C",Arr);
+console.log(Arra);
+
+
+
+
+
+
+
+
+
+
+
+
