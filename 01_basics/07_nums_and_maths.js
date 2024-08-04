76aa8f2dec77f0283f22be9ad6e674505860e729
Sujit Kumar <sujitku5619@gmail.com>
2024-08-04 04:36:36 +0000

    nums and maths

--- 01_basics/07_nums_and_maths.js
+++ 01_basics/07_nums_and_maths.js
@@ -1,9 +1,71 @@
-const score = 400
+/*const score = 400
 console.log(score);
 
 const newscore = new Number (200)
 
-console.log(newscore.toString());
+console.log(newscore.toString().length);
+console.log(newscore.toFixed(5));
+
+
+
+const neo = 123.736
+console.log(neo.toPrecision(4));
+
+//const hun = 100000000000
+console.log(hun.toLocaleString('en-IN'));
+
+*/
+
+//+++++++++++ MATHS ++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-6));
+
+console.log(Math.round(6762.67573));
+
+console.log(Math.ceil(6.9));
+
+console.log(Math.floor(5.9));
+
+
+console.log(Math.sqrt(7863));
+
+
+console.log(Math.min(2, 3 , 6, 1,9));
+
+console.log(Math.max(9,3,6,10,99,999));
+
+
+console.log(Math.random()); // random gives value between 0 and 1
+
+console.log((Math.random()*10)+1); // by adding 1 we ensure that it gives us min 1
+
+
+const min = 23
+const max = 40
+
+console.log(Math.floor(Math.random() *(max - min + 1) +min));
+
+// floor gives us min value
+// random gives us random values
+// 1 is added so that atleast it give us 1 and not 0
+// here (Math.floor) gives us min vaule (Maths.random) gives us random values (*(max-min +1)) it is given so that the value should be between max and min vaule and (+1) so that atleast it give us 1 and not 0 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
