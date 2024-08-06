80a6c001c4dca60a67c921ccc1889223707d0434
Sujit Kumar <sujitku5619@gmail.com>
2024-08-06 06:46:05 +0000

    objects

---
+++ 02_basics/04_OBJECTS.js
@@ -0,0 +1,41 @@
+// const user = new object()
+
+const newuser = {}
+
+// console.log(newuser);
+
+newuser.name = "Sujit",
+newuser.email = "suji@gmail.com",
+newuser.password = "1356"
+
+
+// console.log(newuser);
+
+const User = {
+    email: "13757",
+    Fullname :{
+        Userfullname:{
+            firstname:"sujit",
+            lastname:"kumar"
+        }
+    }
+}
+
+// console.log(User);
+
+ // console.log(User.Fullname);
+
+// console.log(User.Fullname.Userfullname);
+
+
+const alpha = {1:"a",2:"b"}
+const beta = {3:"c",4:"d"}
+
+const gamma = Object.assign({},alpha,beta)
+
+console.log(gamma);
+
+
+
+
+
