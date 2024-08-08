35c98568ca6a0a3dc7cced8a5f730a3fda0a6dca
Sujit Kumar <sujitku5619@gmail.com>
2024-08-08 09:05:58 +0000

    truthy and falsy

---
+++ 04_control_flow/03_truth-falsy.js
@@ -0,0 +1,18 @@
+const useremail = ""
+if (useremail) {
+    console.log("welocme");
+    
+    
+}else{
+    console.log("Access denied");
+    
+}
+// Some values are thruthy and some values are falsy
+
+// falsy values
+
+// NaN, 0 , -0, null , undefined, false , "" , Bigint 0n
+
+// thruthy value
+
+// {} , "0" , 'false' , " " , [] , {} , function(){}
