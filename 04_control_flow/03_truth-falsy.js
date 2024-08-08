4f7739d7644df4a3077fb6ab2445c5a34d2955f2
Sujit Kumar <sujitku5619@gmail.com>
2024-08-08 16:22:11 +0000

    truthy and falsy

--- 04_control_flow/03_truth-falsy.js
+++ 04_control_flow/03_truth-falsy.js
@@ -15,4 +15,43 @@ if (useremail) {
 
 // thruthy value
 
-// {} , "0" , 'false' , " " , [] , {} , function(){}
+// {} , "0" , 'false' , " " , [] , {} , function() {}
+
+// Cheacking if emp is empty or not in Array
+
+if(useremail.length === 0){
+    console.log("Array is empty");
+    
+}
+
+
+// Cheacking if emp is empty or not in object
+
+const emp = "Sujit"
+if(Object.keys(emp).length === 0) {
+    console.log("empty");
+    
+}else{
+    console.log("it is not empty");
+    
+}
+
+// Nullish coalescing operator (??) null , undefined
+
+let val1
+let val2
+val1 = 8 ?? 10
+val2 = 7 ?? undefined
+
+console.log(val1);
+console.log(val2);
+
+
+
+// terniary operator (?)
+
+const price = 100
+price <= 200 ? console.log("less then 200"): console.log("more then 200");
+
+
+
