9ee903233b80904a36a69087d7a4a0cb2704b4c2
Sujit Kumar <sujitku5619@gmail.com>
2024-08-08 04:05:47 +0000

    if-else statement

---
+++ 04_control_flow/01_if-else_statement.js
@@ -0,0 +1,50 @@
+// if statement
+if(3 != "3"){
+    console.log("true");
+}
+
+
+
+// if else statement
+
+if(3 != "3"){
+    console.log("true");
+}
+else {
+        console.log("Chala ja ");
+}      
+
+
+
+
+// if else statement within function 
+
+(  function lelebhai  () {
+if(3 != "3"){
+    console.log("true");
+}
+else {
+        console.log("Chala ja ");
+}      
+        
+ }) ()
+
+
+ // nesting in if - else statement
+ 
+ const balance = 200000
+ if(balance === 0 ){
+    console.log("Insufficient balance");
+    
+ } else if(balance === 100){
+    console.log("Sufficient balance");
+    
+ } else if(balance === 50000){
+    console.log("your current  balance is equal 50000 ");
+
+    
+ } else {
+    console.log("your current  balance is more than 100000");
+    
+ 
+ }
