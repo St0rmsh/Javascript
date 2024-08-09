1568cda2825b1e633fa06a3b71c01461cd5ac367
Sujit Kumar <sujitku5619@gmail.com>
2024-08-09 04:02:41 +0000

    one

--- 05_itreations/one.js
+++ 05_itreations/one.js
@@ -3,18 +3,18 @@
 
 for (let elem = 0; elem <= 10 ; elem++) {
     if(elem == 7){
-        console.log("7 thala for a reason");
+       // console.log("7 thala for a reason");
         
     }
     const element = elem;
-    console.log(element);
+   // console.log(element);
     
     
 }
 // creating 1 to 10 table using for loop
 
 for (let i = 0; i <= 10; i++) {
-    console.log(`outer value is ${i}`);
+    // console.log(`outer value is ${i}`);
     
     
     
@@ -30,18 +30,63 @@ for (let i = 0; i <= 10; i++) {
 // creating 1 to 20 table using for loop 
 
 for (let i = 0; i <= 20; i++) {
-    console.log(`outer  val ${i}`);
+    // console.log(`outer  val ${i}`);
 
 
 
     for (let j = 0; j <= 10; j++) {
-        console.log(i + '*' + j + '=' + i*j);
+        //console.log(i + '*' + j + '=' + i*j);
         
         
     }
     
     
 }
+
+//  Array  in for loop 
+
+const myArray = ["Hulk","Ironman","Captain"]
+// console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+
+
+// break and continue
+
+
+for (let index = 1; index <= 100; index++) {
+    if (index == 7) {
+        console.log(`7 Thala for a reason`);
+        break
+        
+    }
+    const element = index;
+    console.log(element);
+    
+    
+}
+
+
+
+
+for (let index = 1; index <= 100; index++) {
+    if (index == 7) {
+        console.log(`7 Thala for a reason`);
+        continue
+        
+    }
+    const element = index;
+    console.log(element);
+    
+    
+}
+
+
 
 
 
