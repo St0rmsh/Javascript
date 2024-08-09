49b984e1f03d9dccddc71ac4f991bc97d65e7ddb
Sujit Kumar <sujitku5619@gmail.com>
2024-08-09 04:02:55 +0000

    loops

---
+++ 05_itreations/three.js
@@ -0,0 +1,48 @@
+// for of loop
+
+
+let arr = [2,5,6,3,2]
+
+for (const num of arr) {
+    //console.log(num);
+    
+    
+}
+
+
+const greeting = "hello world!"
+
+for (const greet of greeting) {
+   // console.log(`for all ${greet}`);
+    
+    
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN','INDIA')
+map.set('RUSS','RUSSIA')
+
+// console.log(map);
+
+for (const [key , element] of map) {
+    console.log(key ,':' , element);
+    
+    
+}
+
+
+
+// const myObject ={
+  //  ' game1' : "Gta",
+  //  'game2' : "Nfs"
+// }
+
+// for (const [key, value] of myObject) {
+   //console.log(key,":",value);
+    
+    
+// }
+
+
