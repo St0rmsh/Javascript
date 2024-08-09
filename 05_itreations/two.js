e3c4ad2b217118b05e7849a5da5a2229b04140a7
Sujit Kumar <sujitku5619@gmail.com>
2024-08-09 04:03:12 +0000

    loops

---
+++ 05_itreations/two.js
@@ -0,0 +1,31 @@
+let val = 1
+while (val <=10) {
+    //console.log(`the value is ${val}`);
+    val = val+2
+    
+}
+
+// While loop 
+
+
+
+let  myArray = ["Banner","tony","Steve"]
+let  ar = 0
+while (ar < myArray.length) {
+    // console.log(`the value is ${myArray[ar]}`);
+    ar = ar + 1
+    
+    
+    
+}
+
+// Do while loop 
+
+
+let num = 31
+
+do {
+    console.log(`the number is ${num}`);
+    num++
+
+}while (num <= 20) ;
