f300082572400f9e0196197ad00388a466999ebf
Sujit Kumar <sujitku5619@gmail.com>
2024-08-23 06:59:01 +0530

    Delete 07_project_02/clock.js

--- 07_project_02/clock.js
+++
@@ -1,8 +0,0 @@
-const clock = document.getElementById('clock')
-
-
-setInterval(function(){
-    let date = new Date()
-     // console.log(date.toLocaleTimeString());
-     clock.innerHTML = date.toLocaleTimeString();
-}, 1000)
